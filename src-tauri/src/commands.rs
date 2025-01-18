cc659439be91ddb02875ac889cbf070191fd42b2
Igor <igorlfs@ufmg.br>
2025-01-18 12:53:18 -0300

    feat!: debloating
    Remove joke related answers

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,24 +1,15 @@
-use rand::{thread_rng, Rng};
 use std::env;
 use tauri_plugin_database::commands::messages::get_messages;
 
-use crate::{
-    cipher::{caesar_cipher, vigenere_cipher},
-    gemini::{
-        get_chat_response,
-        lib::{Content, Part},
-    },
-    strings::{
-        alternate_string_case, is_string_a_password_secure, is_string_an_email_address,
-        is_string_ordered,
-    },
+use crate::gemini::{
+    get_chat_response,
+    lib::{Content, Part},
 };
 
 #[tauri::command]
 pub async fn get_bot_reply(message: String, chat_id: i32) -> String {
     let apikey = env::var("APIKEY");
 
-    // TODO we shouldn't always default to gemini
     if let Ok(apikey) = apikey {
         let chat_content = get_messages(chat_id)
             .await
@@ -41,19 +32,5 @@ pub async fn get_bot_reply(message: String, chat_id: i32) -> String {
         };
     }
 
-    const NUM_POSSIBLE_ANSWERS: i32 = 7;
-
-    let message = message.as_str();
-
-    match thread_rng().gen_range(0..=NUM_POSSIBLE_ANSWERS) {
-        0 => String::from("Pong!"),
-        // String-related methods
-        1 => is_string_an_email_address(message),
-        2 => is_string_ordered(message),
-        3 => is_string_a_password_secure(message),
-        4 => alternate_string_case(message),
-        // Cipher-related methods
-        5 => caesar_cipher(message, 13),        // ROT 13
-        _ => vigenere_cipher(message, "syrax"), // Fire && Blood
-    }
+    message
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,5 @@
-pub mod cipher;
 mod commands;
 pub mod gemini;
-pub mod strings;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
